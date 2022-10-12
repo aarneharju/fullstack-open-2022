5b7322687dc2d98ecffa570eb6d0e4d7f635f523
aarneharju <35223549+aarneharju@users.noreply.github.com>
2022-10-12 11:27:33 +0300

    1.4 done - refactored props

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -7,24 +7,23 @@ const Header = (props) => {
 const Content = (props) => {
   return (
     <div>
-      <Part part={props.part1} exercises={props.exercises1} />
-      <Part part={props.part2} exercises={props.exercises2} />
-      <Part part={props.part3} exercises={props.exercises3} />
+      <Part part={props.parts[0]} />
+      <Part part={props.parts[1]} />
+      <Part part={props.parts[2]} />
     </div>
   )
 }
 
 const Part = (props) => {
-  { console.log(props) }
   return (
     < p >
-      {props.part} {props.exercises}
+      {props.part['name']} {props.part['exercises']}
     </p >
   )
 }
 
 const Total = (props) => {
-  return <p>Number of exercises {props.exercises1 + props.exercises2 + props.exercises3}</p>
+  return <p>Number of exercises {props.parts[0]['exercises'] + props.parts[1]['exercises'] + props.parts[2]['exercises']}</p>
 }
 
 const App = () => {
@@ -47,8 +46,8 @@ const App = () => {
   return (
     <div>
       <Header course={course} />
-      <Content part1={parts[0]['name']} part2={parts[1]['name']} part3={parts[2]['name']} exercises1={parts[0]['exercises']} exercises2={parts[1]['exercises']} exercises3={parts[1]['exercises']} />
-      <Total exercises1={parts[0]['exercises']} exercises2={parts[1]['exercises']} exercises3={parts[1]['exercises']} />
+      <Content parts={parts} />
+      <Total parts={parts} />
     </div>
   )
 }
