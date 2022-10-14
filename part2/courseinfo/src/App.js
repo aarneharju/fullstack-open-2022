bf92a819e17d2c02c110978f3ad4834c319aa1d4
aarneharju <35223549+aarneharju@users.noreply.github.com>
2022-10-14 14:23:06 +0300

    2.1 done

--- part2/courseinfo/src/App.js
+++ part2/courseinfo/src/App.js
@@ -5,11 +5,11 @@ const Header = (props) => {
 }
 
 const Content = (props) => {
+  console.log({ props });
+  const partsArray = props.parts.map(part => <Part key={part.id} part={part} />)
   return (
     <div>
-      <Part part={props.parts[0]} />
-      <Part part={props.parts[1]} />
-      <Part part={props.parts[2]} />
+      {partsArray}
     </div>
   )
 }
@@ -26,32 +26,41 @@ const Total = (props) => {
   return <p>Number of exercises {props.parts[0]['exercises'] + props.parts[1]['exercises'] + props.parts[2]['exercises']}</p>
 }
 
+const Course = (props) => {
+  return (
+    <div>
+      <Header course={props.course} />
+      <Content parts={props.course.parts} />
+      <Total parts={props.course.parts} />
+    </div>
+  )
+}
+
 const App = () => {
   const course = {
+    id: 1,
     name: 'Half Stack application development',
     parts: [
       {
         name: 'Fundamentals of React',
-        exercises: 10
+        exercises: 10,
+        id: 1
       },
       {
         name: 'Using props to pass data',
-        exercises: 7
+        exercises: 7,
+        id: 2
       },
       {
         name: 'State of a component',
-        exercises: 14
+        exercises: 14,
+        id: 3
       }
     ]
-  };
+  }
 
-  return (
-    <div>
-      <Header course={course.name} />
-      <Content parts={course.parts} />
-      <Total parts={course.parts} />
-    </div>
-  )
+  return <Course course={course} />
+
 }
 
 export default App
