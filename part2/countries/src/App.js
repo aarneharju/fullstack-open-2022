6d829882135299527218688ddcaefb4f36720c66
aarneharju <35223549+aarneharju@users.noreply.github.com>
2022-11-02 07:54:59 +0200

    2.14 done

--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -9,9 +9,14 @@ function App() {
   const [controlledSearch, setControlledSearch] = useState('');
   const [shownCountriesArray, setShownCountriesArray] = useState([]);
   const [countriesArray, setCountriesArray] = useState([]);
+  const [selectedCountryWeather, setSelectedCountryWeather] = useState({});
+
+  console.log({ selectedCountryWeather });
 
   // Set variables
   const endpoint = 'https://restcountries.com/v3.1/all';
+  const openweather_API_KEY = process.env.REACT_APP_OPENWEATHER_API_KEY;
+  const openweatherAPIendpoint = 'https://api.openweathermap.org/data/2.5/weather?';
 
   // Effects
   useEffect(() => {
@@ -22,6 +27,25 @@ function App() {
         return;
       });
   }, [])
+
+  useEffect(() => {
+    if (shownCountriesArray.length === 1) {
+
+      const openweatherFullFetchUrl = openweatherAPIendpoint + 'lat=' + shownCountriesArray[0].capitalInfo.latlng[0] + '&lon=' + shownCountriesArray[0].capitalInfo.latlng[1] + '&appid=' + openweather_API_KEY + '&units=metric';
+
+      fetch(openweatherFullFetchUrl)
+        .then(result => result.json())
+        .then(data => {
+          console.log({ data });
+          setSelectedCountryWeather(selectedCountryWeather => {
+            return {
+              ...selectedCountryWeather,
+              ...data
+            }
+          });
+        });
+    }
+  }, [shownCountriesArray])
 
   // Functions
   const handleSearch = (event) => {
@@ -42,7 +66,7 @@ function App() {
       <h1>Countries</h1>
       <label htmlFor='search'>Find countries:</label>
       <input type='text' name='search' id='search' value={controlledSearch} onChange={handleSearch} />
-      <Countries shownCountriesArray={shownCountriesArray} handleClick={handleClick} controlledSearch={controlledSearch} countriesArray={countriesArray} />
+      <Countries shownCountriesArray={shownCountriesArray} handleClick={handleClick} controlledSearch={controlledSearch} countriesArray={countriesArray} selectedCountryWeather={selectedCountryWeather} />
     </div>
   );
 }
--- part2/countries/src/components/Countries.js
+++ part2/countries/src/components/Countries.js
@@ -1,24 +1,32 @@
+import { useEffect } from "react";
 import ButtonShow from "./ButtonShow";
 
 const Countries = (props) => {
 
+    console.log('weather', props.selectedCountryWeather);
+
     // Conditional rendering
     if (props.shownCountriesArray.length === 1) {
 
-        const languagesArray = Object.values(props.shownCountriesArray[0].languages).map(language => <li key={language}>{language}</li>);
-        const flagImageUrl = props.shownCountriesArray[0].flags.png;
-
+        const selectedCountry = props.shownCountriesArray[0];
+        const languagesArray = Object.values(selectedCountry.languages).map(language => <li key={language}>{language}</li>);
+        const flagImageUrl = selectedCountry.flags.png;
+        const weatherIconUrl = 'https://openweathermap.org/img/wn/' + props.selectedCountryWeather.weather[0].icon + '@2x.png';
 
         return (
             <div>
-                <h2>{props.shownCountriesArray[0].name.common}</h2>
-                <p>Capital: {props.shownCountriesArray[0].capital}</p>
-                <p>Area: {props.shownCountriesArray[0].area}</p>
+                <h2>{selectedCountry.name.common}</h2>
+                <p>Capital: {selectedCountry.capital}</p>
+                <p>Area: {selectedCountry.area}</p>
                 <p>Languages:</p>
                 <ul>
                     {languagesArray}
                 </ul>
                 <img src={flagImageUrl} />
+                <h3>Weather in {selectedCountry.capital}</h3>
+                <p>Temperature: {props.selectedCountryWeather.main.temp} Celsius</p>
+                <img src={weatherIconUrl} alt='Weather icon' />
+                <p>Wind: {props.selectedCountryWeather.wind.speed} m/s</p>
             </div>
         )
     }
@@ -26,8 +34,6 @@ const Countries = (props) => {
     if (props.shownCountriesArray.length > 1 && props.shownCountriesArray.length <= 10) {
 
         const selectedCountriesNamesAsListItems = props.shownCountriesArray.map(country => <li key={country.name.common}>{country.name.common} <ButtonShow country={country} shownCountriesArray={props.shownCountriesArray} handleClick={props.handleClick} text='Show' /></li>)
-
-        // const selectedCountriesNamesAsListItems = props.shownCountriesArray.map(country => <li key={country.name.common}>{country.name.common} <button onClick={() => handleClick(country)}>ShowIt</button></li>)
 
         return (
             <ul>
